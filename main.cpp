f1909ac0354a7e5c9f13d3b546512143d9b8c3df
Yuwei Xie <948011378@qq.com>
2023-04-15 15:49:07 +0800

    第一次提交测试

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+int main(void) {
+	int a;
+	cin >> a;
+	switch (a) {
+	case 0:
+		cout << "Áã";
+		break;
+	case 1:
+		cout << "Ò¼";
+		break;
+	case 2:
+		cout << "·¡";
+		break;
+	case 3:
+		cout << "Èþ";
+		break;
+	case 4:
+		cout << "ËÁ";
+		break;
+	case 5:
+		cout << "Îé";
+		break;
+	case 6:
+		cout << "Â½";
+		break;
+	case 7:
+		cout << "Æâ";
+		break;
+	case 8:
+		cout << "°Æ";
+		break;
+	case 9:
+		cout << "¾Á";
+		break;
+	default:
+		cout << "ÎÞ";
+		break;
+	}
+
+	return 0;
+}
